66ad492e8c57d774beab065bebbe69ea2c3f0790
sethdorris <sethc.dorris@gmail.com>
2016-06-20 14:51:20 -0600

    Updating routing

---
+++ app/app.routes.ts
@@ -0,0 +1,11 @@
+﻿import { provideRouter, RouterConfig } from '@angular/router';
+import { login } from '../app/app.login';
+
+
+export const routes: RouterConfig = [
+    { path: '/login', component: login }
+];
+
+export const APP_ROUTER_PROVIDERS = [
+    provideRouter(routes)
+];
--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,7 @@
 ﻿import { bootstrap } from '@angular/platform-browser-dynamic';
+import { APP_ROUTER_PROVIDERS } from './app.routes';
 import { AppComponent } from './app.component';
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [
+    APP_ROUTER_PROVIDERS
+]);
